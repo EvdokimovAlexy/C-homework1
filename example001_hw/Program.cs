a442865288af2f4ff5eadc426a96d2f5483b9518
Prometheus <poligraphich@gmail.com>
2022-08-31 20:20:19 +0300

    add example001 max_min

---
+++ example001_hw/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+int numberB = Convert.ToInt32 (Console.ReadLine());
+int max = numberA;
+int min = numberB;
+if (numberB > max) max = numberB;
+
+{
+    Console.Write("Наибольшее число "); 
+    Console.WriteLine(max);
+}
+
+if (numberA < min) min = numberA;
+{
+    Console.Write("Наименьшее число "); 
+    Console.WriteLine(min);
+}
