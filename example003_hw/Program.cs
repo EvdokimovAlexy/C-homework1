e1a853cbff8d749b67cda7ed884498ae1f26791f
Prometheus <poligraphich@gmail.com>
2022-09-01 20:22:54 +0300

    add example 003

---
+++ example003_hw/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите любое число");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
