c41c824736ada331067f1c5d306a3b9c80770d16
Prometheus <poligraphich@gmail.com>
2022-09-02 19:29:01 +0300

    add example004

---
+++ example004_hw/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите любое число");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+
+for (int index = 1; index < numberA; index++)
+{
+    if (index % 2 == 0)
+    {
+    Console.WriteLine(index);
+    }
+}
+   
